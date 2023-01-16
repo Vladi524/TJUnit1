05727687aed2d379ee75ba197b8afa100b14ceb5
Vladi524 <5172777@gmail.com>
2023-01-16 16:00:46 +0300

    v 1.1

--- VariablesTheme.java
+++ VariablesTheme.java
@@ -1,88 +1,87 @@
 public class VariablesTheme {
     public static void main(String[] args) {
-        task1();
-        task2();
-        task3();
-        task4();
-        task5();
-        task6();
-        task7();
-        task8();
-        task9();
+        createVariables();
+        calculateDiscountPrice();
+        printJava();
+        printMinMax();
+        swapVariables();
+        printSymbolCode();
+        printAsciiDuke();
+        printDigits();
+        printTime();
     }
 
-    public static void task1() {
-        byte processorCore = 4;
+    private static void createVariables() {
+        System.out.println("1. Создание переменных и вывод их значений на консоль\n");
+
+        byte processorCoreQuantity = 4;
         boolean isComputerNote = true;
         short memorySize = 8192;
         int coreFrequency = 2200;
         long diskVolume = 2147483648L;
         float screenDiag = 14.1f;
         double accVolume = 5000.15d;
-        String computerBrand = "Samsung";
+        char modelIndex = 'S';
 
-        System.out.println("1. Создание переменных и вывод их значений на консоль\n");
-        System.out.println("Количество ядер в процессоре: " + processorCore + " шт");
+        System.out.println("Количество ядер в процессоре: " + processorCoreQuantity + " шт");
         System.out.println("Этот компьютер ноутбук? " + isComputerNote);
         System.out.println("Обьем оперативной памяти: " + memorySize + " Мгб");
         System.out.println("Частота каждого ядра: " + coreFrequency + "Мгц");
         System.out.println("Объем жесткого диска: " + diskVolume + " бит");
         System.out.println("Диагональ экрана: " + screenDiag + "дюймов");
         System.out.println("Емкость батареи: " + accVolume  + " mAh");
-        System.out.println("Марка ноутбука: " + computerBrand);
+        System.out.println("Индекс модели: " + modelIndex);
     }
 
-    public static void task2() {
+    private static void calculateDiscountPrice() {
+        System.out.println("\n2. Расчет стоимости товара со скидкой");
+
         int penPrice = 100; 
         int bookPrice = 200;
         int discount = 11;
-        int PriceWithDiscount;
+        int discountPrice = 0;
 
-        System.out.println("\n2. Расчет стоимости товара со скидкой");
         discount = (penPrice + bookPrice) / 100 * discount;
-        PriceWithDiscount = penPrice + bookPrice - discount;
+        discountPrice = penPrice + bookPrice - discount;
         System.out.println("Сумма скидки: " + discount + " руб.");
-        System.out.println("Общая стоимость товаров со скидкой: " + PriceWithDiscount + " руб."); 
+        System.out.println("Общая стоимость товаров со скидкой: " + discountPrice + " руб."); 
     }
 
-    public static void task3() {
-        final char J = 'J';
-        final char VCAP = 'V';
-        final char V = 'v';
-        final char A = 'a';
-
+    private static void printJava() {
         System.out.println("\n3. Вывод на консоль слова JAVA");
-        System.out.format("%4c%4c%3c%3$6c%2$3c\n", J, A, V);
-        System.out.format("%1$4c%2$3c%2$2c%3$3c%3$4c%2$3c%2$2c\n", J, A, V);
-        System.out.format("%c%1$3c%2c%2$c%2$c%2$c%2$c%3c%3$2c%2$3c%2$c%2$c%2$c%2$c\n", J, A, VCAP);
-        System.out.format("%2c%1$c%2c%2$6c%3c%2$3c%2$6c", J, A, VCAP);
+        System.out.println("   J   a  v     v  a");
+        System.out.println("   J  a a  v   v  a a");
+        System.out.println("J  J aaaaa  V V  aaaaa");
+        System.out.println(" JJ a     a  V  a     a");
     }
 
-    public static void task4() {
-        byte b = 127;
-        short s = 32767;
-        int i = 2147483647;
-        long l = 9223372036854775807L;
+    private static void printMinMax() {
+        System.out.println("\n4. Отображение min и max значений числовых типов данных");
 
-        System.out.println("\n\n4. Отображение min и max значений числовых типов данных");
-        System.out.printf("Byte: %d %d %d", b, ++b, --b);
-        System.out.printf("\nShort: %d %d %d", s, ++s, --s);
-        System.out.printf("\nInteger: %d %d %d", i, ++i, --i);
-        System.out.printf("\nLong: %d %d %d\n", l, ++l, --l);
+        byte maxByte = 127;
+        short maxShort = 32767;
+        int maxInt = 2147483647;
+        long maxLong = 9223372036854775807L;
+
+        System.out.printf("Byte: %d %d %d", maxByte, ++maxByte, --maxByte);
+        System.out.printf("\nShort: %d %d %d", maxShort, ++maxShort, --maxShort);
+        System.out.printf("\nInteger: %d %d %d", maxInt, ++maxInt, --maxInt);
+        System.out.printf("\nLong: %d %d %d\n", maxLong, ++maxLong, --maxLong);
     }
 
-    public static void task5() {
+    private static void swapVariables() {
+        System.out.println("\n5. Перестановка значений переменных");
+
         int a = 2;
         int b = 5;
-        int tempStorage;
+        int temp = 0;
 
-        System.out.println("\n5. Перестановка значений переменных");
         System.out.println("Исходные значения: a = " + a + ", b = " + b);
 
         System.out.println("\nС помощью третьей переменной: ");
-        tempStorage = a;
+        temp = a;
         a = b;
-        b = tempStorage;
+        b = temp;
         System.out.println("Результат: a = " + a + ", b = " + b);
 
         System.out.println("\nC помощью арифметических операций: ");
@@ -98,44 +97,50 @@ public class VariablesTheme {
         System.out.println("Результат: a = " + a + ", b = " + b);
     }
 
-    public static void task6() {
-        final char sim35 = '#';
-        final char sim38 = '&';
-        final char sim64 = '@';
-        final char sim94 = '^';
-        final char sim95 = '_';
-
+    private static void printSymbolCode() {
         System.out.println("\n6. Вывод символов и их кодов");
-        System.out.println((int) sim35 + " " + sim35);
-        System.out.println((int) sim38 + " " + sim38);
-        System.out.println((int) sim64 + " " + sim64);
-        System.out.println((int) sim94 + " " + sim94);
-        System.out.println((int) sim95 + " " + sim95);
+
+        char symbol35 = '#';
+        char symbol38 = '&';
+        char symbol64 = '@';
+        char symbol94 = '^';
+        char symbol95 = '_';
+
+        System.out.println((int) symbol35 + " " + symbol35);
+        System.out.println((int) symbol38 + " " + symbol38);
+        System.out.println((int) symbol64 + " " + symbol64);
+        System.out.println((int) symbol94 + " " + symbol94);
+        System.out.println((int) symbol95 + " " + symbol95);
     }
 
-    public static void task7() {
-        char sl = '/';
-        char backSl = '\\';
-        char br = '(';
-        char backBr = ')';
-        char underscr = '_';
-
+    private static void printAsciiDuke() {
         System.out.println("\n7. Вывод в консоль ASCII-арт Дюка");
-        System.out.println("    " + sl + backSl + "    ");
-        System.out.println("   " + sl + "  " + backSl + "   ");
-        System.out.println("  " + sl + underscr + br + " " + backBr + backSl + "  ");
-        System.out.println(" " + sl + "      " + backSl + " ");
-        System.out.println("" + sl + underscr + underscr + underscr + sl + backSl + underscr +
-                 underscr + underscr + backSl);
+
+        char slash = '/';
+        char backSlash = '\\';
+        char openBracket = '(';
+        char closeBracket = ')';
+        char underScore = '_';
+
+        System.out.println("    " + slash + backSlash + "    ");
+        System.out.println("   " + slash + "  " + backSlash + "   ");
+        System.out.println("  " + slash + underScore + openBracket + " " + closeBracket + 
+                backSlash + "  ");
+        System.out.println(" " + slash + "      " + backSlash + " ");
+        System.out.println("" + slash + underScore + underScore + underScore + slash + backSlash + 
+                underScore + underScore + underScore + backSlash);
     }
 
-    public static void task8() {
-        int number = 123;
-        int hundreds;
-        int tens;
-        int ones;
-
+    private static void printDigits() {
         System.out.println("\n8. Вывод количества сотен, десятков и единиц числа");
+
+        int number = 123;
+        int hundreds = 0;
+        int tens = 0;
+        int ones = 0;
+        int allNumberAddition = 0;
+        int allNumberMultiplication = 0;
+
         System.out.println("Число " + number + " содержит:");
         hundreds = number / 100;
         number %= 100;
@@ -144,20 +149,22 @@ public class VariablesTheme {
         System.out.println(hundreds + " сотня");
         System.out.println(tens + " десяткa");
         System.out.println(ones + " единицы");
-        number = hundreds + tens + ones;
-        System.out.println("Сумма его цифр = " + number);
-        number = hundreds * tens * ones;
-        System.out.println("Произведение = " + number);
+        allNumberAddition = hundreds + tens + ones;
+        System.out.println("Сумма его цифр = " + allNumberAddition);
+        allNumberMultiplication = hundreds * tens * ones;
+        System.out.println("Произведение = " + allNumberMultiplication);
     }
 
-    public static void task9() {
-        int secs = 86399;
-        int mins;
-        int hours;
-
+    private static void printTime() {
         System.out.println("\n9. Вывод времени");
-        mins = secs / 60;
-        secs %= 60;
+
+        int initialSecs = 86399;
+        int secs = 0;
+        int mins = 0;
+        int hours = 0;
+
+        mins = initialSecs / 60;
+        secs = initialSecs % 60;
         hours = mins / 60;
         mins %= 60;
         System.out.println(hours + ":" + mins + ":" + secs);
